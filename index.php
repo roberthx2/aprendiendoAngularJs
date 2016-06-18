ed445b72f258758a75c32dde95b7ae94d2dea199
Roberth Riera <riera_1992@hotmail.com>
2016-06-18 19:31:51 -0400

    Comit principal, Hola mundo

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html ng-app>
+<head>
+	<meta charset="UTF-8">
+	<title>Hola mundo con angularJS</title>
+	<script src="angular/angular.js">  </script>
+</head>
+<body>
+	{{"Hola " + "Mundo"}}
+</body>
+</html>
