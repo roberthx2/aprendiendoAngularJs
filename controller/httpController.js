5b42df94d1d66228efe1916c643b30f429134849
Roberth Riera <riera_1992@hotmail.com>
2016-06-19 09:36:09 -0400

    AJAX con  parte 2

--- controller/httpController.js
+++ controller/httpController.js
@@ -1,6 +1,7 @@
 angular.module("http",[])
 	.controller("httpController", function($scope, $http){
 		$scope.posts = [];
+		$scope.newPost = {};
 		$http.get("http://jsonplaceholder.typicode.com/posts")
 			.success(function(data){
 				console.log(data);
@@ -9,4 +10,19 @@ angular.module("http",[])
 			.error(function(err){
 
 			});
+
+		$scope.addPost = function(){
+			$http.post("http://jsonplaceholder.typicode.com/posts",{
+				title: $scope.newPost.title,
+				body: $scope.newPost.body,
+				userid: $scope.newPost.userid
+			})
+			.success(function(data, status, headers, config){
+				$scope.posts.push($scope.newPost);
+				$scope.newPost = {};
+			})
+			.error(function(error){
+				console.log(error);
+			});
+		}
 	});
