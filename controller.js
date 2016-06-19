91a0e4db535f2c31a9a55342ee78022acb13ae6a
Roberth Riera <riera_1992@hotmail.com>
2016-06-18 20:24:28 -0400

    Directices ng-model, ng-click y ng-repeat

--- controller.js
+++ controller.js
@@ -1,4 +1,19 @@
  var app = angular.module("MiModulo",[]);
  app.controller("MiController", function($scope){
  	$scope.nombre = "Roberth";
+ 	$scope.nuevoComentario = {};
+ 	$scope.comentarios = [
+	 	{
+	 		comentario: "Buen tutorial",
+	 		username: "roberthx2" 
+	 	},
+	 	{
+	 		comentario: "Malisimo el tutorial",
+	 		username: "vegeta"
+	 	}
+ 	];
+ 	$scope.agregarComentario = function(){
+ 		$scope.comentarios.push($scope.nuevoComentario);
+ 		$scope.nuevoComentario = {};
+ 	}
  });
--- index.php
+++ index.php
@@ -7,6 +7,16 @@
 	<script src="controller.js"></script>
 </head>
 <body ng-controller="MiController">
-	{{"Hola " + nombre}}
+	<label>Comentario</label>
+	<input type="text" ng-model="nuevoComentario.comentario"><br>
+	<label>Username</label>
+	<input type="text" ng-model="nuevoComentario.username"><br>
+	<button ng-click="agregarComentario()">Agregar Comentario</button>
+	<h3>Comentarios</h3>
+	<ul>
+		<li ng-repeat="comentario in comentarios">
+			{{comentario.comentario}} - <strong>{{comentario.username}}</strong>
+		</li>
+	</ul>
 </body>
 </html>
